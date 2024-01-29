6b53c48f1bceeea8c1a90bf1584b8f7626f53a6c
Tyler <tyleraorso@icloud.com>
2024-01-28 21:13:30 -0800

    Initial Commit

---
+++ forms.py
@@ -0,0 +1,38 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, SelectField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditorField
+
+
+# WTForm for creating a blog post
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+# TODO: Create a RegisterForm to register new users
+class RegisterForm(FlaskForm):
+    email = StringField(label="Email", validators=[DataRequired()])
+    password = PasswordField(label="Password", validators=[DataRequired()])
+    name = StringField(label="Name/Username", validators=[DataRequired()])
+    submit = SubmitField("Sign me up!")
+
+# TODO: Create a LoginForm to login existing users
+class LoginForm(FlaskForm):
+    email = StringField(label="Email", validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label="Log me In!")
+
+# TODO: Create a CommentForm so users can leave comments below posts
+class CommentForm(FlaskForm):
+    comment = CKEditorField("Comment", validators=[DataRequired()])
+    submit = SubmitField(label="Submit My Comment")
+
+class Suggest_Edit(FlaskForm):
+    edit_type = SelectField("Please select what type of edit you would like to suggest:", choices=["Error in Article", "New Feature", "Bug fix", "Other"], validators=[DataRequired()])
+    edit_text = CKEditorField("Please enter more info in the box below. If the edit is a bug fix or article, please explain exactly where the error is or what article and paragraph the bug/mistake occurs", validators=[DataRequired()])
+    other_information = StringField(label="Please type anything else that you want to be changed (Not Required)")
+    submit = SubmitField(label="Submit my suggested edit (Suggested Edits are private and your username will not be revealed)")
